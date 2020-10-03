5642582be9451358a1c51165d88b0de20b08fd4d
renatocinttra <renatocinttra@gmail.com>
2020-10-03 10:09:14 -0300

    Added the method GET for pageStudy and pageGiveClasses. Was created two variable [filters and data] for take the request arguments and append list Proffys.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -38,22 +38,53 @@ proffys = [
     }
 ]
 
+subjects = [
+    'Artes',
+    'Biologia',
+    'Ciência',
+    'Educação Física',
+    'Física',
+    'Geografia',
+    'História',
+    'Matemática',
+    'Português',
+    'Química'
+]
+
+weekdays = [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado'
+]
+
 
 @app.route('/')
 def pageLanding():
     return render_template("index.html")
 
 
-@app.route('/study')
+@app.route('/study', methods=['GET'])
 def pageStudy():
-    return render_template("study.html", proffys=proffys)
+    filters = request.args
+    # Try change to loop.index in the conditional of page study.html
+    return render_template("study.html", proffys=proffys, filters=filters, subjects=subjects, weekdays=weekdays)
 
 
-@app.route('/give-classes')
+@app.route('/give-classes', methods=['GET'])
 def pageGiveClasses():
-    return render_template("give-classes.html")
+    data = request.args
+    # Try change to loop.index in the conditional of page give-classes.html
+    if len(data) != 0:
+        proffys.append(data)
+        return redirect(url_for('pageStudy'))
+    else:
+        return render_template("give-classes.html", subjects=subjects, weekdays=weekdays, data=data)
 
 
-# Video_04 = 01h21m00s
+# Video_05 = 00h00m00s
 
 app.run(debug=True)
